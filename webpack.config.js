b4ca2818ad145c571e85617f3f3ee8188c91172f
Ian Turgeon <iturgeon@gmail.com>
2018-11-16 18:10:34 -0500

    optimize assets, remove jquery from player

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
-// load the reusable legacy webpack config from materia-widget-dev
-let baseConfig = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig()
+const path = require('path')
+const srcPath = path.join(__dirname, 'src') + path.sep
+const widgetWebpack = require('materia-widget-development-kit/webpack-widget')
+const entries = widgetWebpack.getDefaultEntries()
 
-let path = require('path')
-srcPath = path.join(__dirname, 'src')
-baseConfig.entry['draw.js'] = [path.join(srcPath, 'draw.coffee')]
+entries['player.js'] = [
+	srcPath+'player.coffee',
+	srcPath+'draw.coffee'
+]
 
-module.exports = baseConfig
+entries['creator.js'] = [
+	srcPath+'creator.coffee',
+	srcPath+'draw.coffee',
+	srcPath+'spectrum.custom.js'
+]
+
+// options for the build
+let options = {
+	entries: entries
+}
+
+module.exports = widgetWebpack.getLegacyWidgetBuildConfig(options)
