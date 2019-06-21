981b479a6ddf2bfdf1189342fb9f149c5f9689ab
Weining Li <weining.li@ucf.edu>
2019-06-21 08:18:32 -0400

    added creator and player guides

---
+++ src/_helper-docs/player.md
@@ -0,0 +1,24 @@
+# Overview #
+
+Labeling is a quiz tool which requires students to correctly identify certain parts of an image by placing labels on their matches within the image.
+
+![labeling screen](assets/widget_guides_labeling.jpg "labeling screen")
+
+## Overview ##
+
+1. Title of the widget
+2. Term list
+3. Labels
+4. Image to be labeled
+5. Points
+6. Done button
+
+## Details ##
+
+### Labels ###
+
+Drag labels from the **Term List** onto the corresponding points on the image. Labels will snap into place as you move them over the image. Some points may expect more than one label.
+
+### Submitting ###
+
+When you've labeled all the points click **Done** to submit your answered and review how you did.
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,62 @@
+const fs = require('fs')
 const path = require('path')
-const srcPath = path.join(__dirname, 'src') + path.sep
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const marked = require('meta-marked')
 const widgetWebpack = require('materia-widget-development-kit/webpack-widget')
+const rules = widgetWebpack.getDefaultRules()
+const copy = widgetWebpack.getDefaultCopyList()
+
+const srcPath = path.join(__dirname, 'src') + path.sep
+const outputPath = path.join(process.cwd(), 'build')
+
+const customCopy = copy.concat([
+	{
+		from: `${srcPath}/_helper-docs/assets`,
+		to: `${outputPath}/guides/assets`,
+		toType: 'dir'
+	}
+])
+
 const entries = widgetWebpack.getDefaultEntries()
 
 entries['player.js'] = [
-	srcPath+'player.coffee',
-	srcPath+'draw.coffee'
+	srcPath + 'player.coffee',
+	srcPath + 'draw.coffee'
 ]
 
 entries['creator.js'] = [
-	srcPath+'creator.coffee',
-	srcPath+'draw.coffee',
-	srcPath+'spectrum.custom.js'
+	srcPath + 'creator.coffee',
+	srcPath + 'draw.coffee',
+	srcPath + 'spectrum.custom.js'
 ]
 
-// options for the build
-let options = {
+entries['guides/guideStyles.css'] = [
+	srcPath + '_helper-docs/guideStyles.scss'
+]
+
+const options = {
+	copyList: customCopy,
 	entries: entries
 }
 
-module.exports = widgetWebpack.getLegacyWidgetBuildConfig(options)
+const generateHelperPlugin = name => {
+	const file = fs.readFileSync(path.join(__dirname, 'src', '_helper-docs', name+'.md'), 'utf8')
+	const content = marked(file)
+
+	return new HtmlWebpackPlugin({
+		template: path.join(__dirname, 'src', '_helper-docs', 'helperTemplate'),
+		filename: path.join(outputPath, 'guides', name+'.html'),
+		title: name.charAt(0).toUpperCase() + name.slice(1),
+		chunks: ['guides'],
+		content: content.html
+	})
+}
+
+let buildConfig = widgetWebpack.getLegacyWidgetBuildConfig(options)
+
+buildConfig.plugins.unshift(generateHelperPlugin('creator'))
+buildConfig.plugins.unshift(generateHelperPlugin('player'))
+
+module.exports = buildConfig
+
+// module.exports = widgetWebpack.getLegacyWidgetBuildConfig(options)
