584e6c33d99c1639fa71631d6057dd04392dce42
Emanuel Cortes Lugo <emanuelcodes@gmail.com>
2021-03-03 14:02:45 -0500

    Working on rendering a 3D object

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,10 @@ const entries = {
 		path.join(__dirname, 'src', 'creator.js')
 	],
 	'player.js': [
+		path.join(__dirname, 'src/three', 'three.min.js'),
 		path.join(__dirname, 'src', 'draw.js'),
-		path.join(__dirname, 'src', 'player.js')
+		path.join(__dirname, 'src', 'player.js'),
+		path.join(__dirname, 'src', 'player.html')
 	],
 	'creator.css': [
 		path.join(__dirname, 'src', 'creator.html'),
