cb6f16f001b5b0c8b7347c9da766e600014f284f
Emanuel Cortes Lugo <emanuelcodes@gmail.com>
2021-03-12 16:34:33 -0500

    Add arrow in 3D space, also using canvas draw one in 2D space.
    Former-commit-id: a45d8e911192195ebab20d2876f8a70f7ea28a80

--- src/draw3D.js
+++ src/draw3D.js
@@ -6,14 +6,16 @@ console.log('Beginning of draw3D.js');
 
 const scene = new THREE.Scene();
 // scene.background = new THREE.Color(0xcccccc);
-
-const camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000
+let cameraDimensions = 10;
+const camera = new THREE.OrthographicCamera(
+	-cameraDimensions,
+	cameraDimensions,
+	cameraDimensions,
+	-cameraDimensions,
+	-cameraDimensions * 4,
+	cameraDimensions * 4
 );
-camera.position.set(0, 0, 10);
+camera.position.set(0, 0, 5);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setClearColor(0x000000, 0);
@@ -22,27 +24,47 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 const canvas =
 	document.getElementById('board').appendChild(renderer.domElement);
-
 canvas.setAttribute('id', '3D');
 
-// window.addEventListener('resize', function () {
-// 	camera.aspect = window.innerWidth / window.innerWidth;
-// 	camera.updateProjectionMatrix();
-// 	renderer.setSize(window.innerWidth, window.innerHeight);
-// }, false);
-
-const geometry = new THREE.BoxGeometry(1, 1, 15);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+// BoxGeometry(x, y, z)
+// BoxGeometry(length, width, height)
+const geometryRectangle = new THREE.BoxGeometry(0.5, 0.5, 4);
+const materialBox = new THREE.MeshBasicMaterial({ color: 0x000ccc, wireframe: false });
+const cube = new THREE.Mesh(geometryRectangle, materialBox);
 cube.castShadow = true;
 cube.receiveShadow = false;
-cube.position.set(-2, 0, 0);
-scene.add(cube);
+cube.position.set(0, 0, 0);
+cube.rotation.x += 0;
+cube.rotation.y += 0;
+cube.rotation.z += 5;
+// scene.add(cube);
+
+// CylinderGeometry(radiusTop, radiusBottom, height, radialSegments, heightSegments)
+const geometryCylinder = new THREE.CylinderGeometry(.3, .3, 4, 16, 16);
+const materialCylinder = new THREE.MeshBasicMaterial({ color: 0x00cccc, wireframe: false });
+const cylinder = new THREE.Mesh(geometryCylinder, materialCylinder);
+cylinder.castShadow = true;
+cylinder.receiveShadow = false;
+cylinder.position.set(0, 4.5, 0);
+cylinder.rotation.x += 0;
+
+scene.add(cylinder);
+
+// ConeGeometry(radius, height, radialSegments, heightSegments)
+const geometryCone = new THREE.ConeGeometry(.8, 1.5, 16);
+const materialCone = new THREE.MeshBasicMaterial({ color: 0x00cccc, wireframe: false });
+const cone = new THREE.Mesh(geometryCone, materialCone);
+cone.castShadow = true;
+cone.receiveShadow = false;
+cone.position.set(0, 7, 0);
+scene.add(cone);
 
 const animate = function () {
 	requestAnimationFrame(animate);
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	// cone.rotation.x += 0.01;
+	// cone.rotation.y += 0.01;
+	// cylinder.rotation.x += 0.01;
+	// cylinder.rotation.y += 0.01;
 
 	renderer.render(scene, camera);
 }
