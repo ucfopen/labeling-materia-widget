3a13515103a7a1263e35c8ee7f4fff9e9f51eade
Emanuel Cortes Lugo <emanuelcodes@gmail.com>
2021-09-07 15:48:31 -0400

    Builds and loads in docker
    Former-commit-id: 7f25d45ce7af6f9da868c27a6ecdcacf5e5b4fe0

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,10 @@ const widgetWebpack = require('materia-widget-development-kit/webpack-widget')
 const copy = widgetWebpack.getDefaultCopyList()
 const rules = widgetWebpack.getDefaultRules()
 
-const webpack = require('webpack');
-
 const entries = {
 	'creator.js': [
 		path.join(__dirname, 'src', 'spectrum.custom.js'),
 		path.join(__dirname, 'src', 'draw.js'),
-		path.join(__dirname, 'src', 'core3D.js'),
 		path.join(__dirname, 'src', 'creator.js')
 	],
 	'player.js': [
@@ -59,31 +56,6 @@ const customCopy = copy.concat([
 	},
 ])
 
-/*
-I get different ERRORS based on how "modules": "", is configure.
-"auto" --> Uncaught ReferenceError: exports is not defined at core3D.js:3
-
-"commonjs" --> core3D.js:3 Uncaught ReferenceError: exports is not defined at core3D.js:3
-								|==> In function setUp3DEnvironment()
-					---> ReferenceError: require is not defined at creator.js:3115
-*/
-const useBabelLoader = {
-	test: /\.js$/,
-	exclude: /node_modules/,
-	use: {
-		loader: 'babel-loader',
-		options: {
-			presets: ['@babel/preset-env'],
-			plugins: [
-				["@babel/plugin-transform-modules-commonjs", {
-					"allowTopLevelThis": true,
-					"loose": true,
-				}],
-			],
-		}
-	}
-}
-
 const moduleRules = [
 	rules.loaderDoNothingToJs,
 	rules.loadAndCompileMarkdown,
@@ -91,7 +63,6 @@ const moduleRules = [
 	rules.loadHTMLAndReplaceMateriaScripts,
 	rules.loadAndPrefixCSS,
 	rules.loadAndPrefixSASS,
-	useBabelLoader,
 ]
 
 const options = {
@@ -103,14 +74,6 @@ const options = {
 let buildConfig = widgetWebpack.getLegacyWidgetBuildConfig(options)
 
 // possible CDN future, would require an intermediate 'loading resources' screen due to long load times
-// buildConfig.externals = {
-// 	'https://unpkg.com/three@0.124.0/build/three.module.js': 'https://unpkg.com/three@0.124.0/build/three.module.js',
-// 	'https://unpkg.com/three@0.124.0/examples/jsm/controls/OrbitControls.js': 'https://unpkg.com/three@0.124.0/examples/jsm/controls/OrbitControls.js',
-// 	'https://unpkg.com/three@0.124.0/examples/jsm/loaders/MTLLoader.js': 'https://unpkg.com/three@0.124.0/examples/jsm/loaders/MTLLoader.js',
-// 	'https://unpkg.com/three@0.124.0/examples/jsm/loaders/OBJLoader.js': 'https://unpkg.com/three@0.124.0/examples/jsm/loaders/OBJLoader.js',
-// 	'https://unpkg.com/three@0.124.0/examples/jsm/libs/stats.module.js': 'https://unpkg.com/three@0.124.0/examples/jsm/libs/stats.module.js'
-// }
-
 buildConfig.externals = {
 	'https://cdnjs.cloudflare.com/ajax/libs/three.js/r124/three.module.min.js': 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r124/three.module.min.js',
 	'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/controls/OrbitControls.js': 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/controls/OrbitControls.js',
@@ -120,4 +83,3 @@ buildConfig.externals = {
 }
 
 module.exports = buildConfig
-// module.exports = widgetWebpack.getLegacyWidgetBuildConfig(options)
